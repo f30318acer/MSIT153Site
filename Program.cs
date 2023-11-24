144e6941758da9258f0e62f8316cd4b1236d5de1
f30318acer <f50318acer@gmail.com>
2023-11-24 21:33:17 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=hw3}/{id?}");
+    pattern: "{controller=Home}/{action=index}/{id?}");
 
 app.Run();
