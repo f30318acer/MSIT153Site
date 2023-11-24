9dbd288817b78badf0f90157645feda0a8acb46e
f30318acer <f50318acer@gmail.com>
2023-11-24 17:31:21 +0800

    update
    update

--- Program.cs
+++ Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=jQuery}/{id?}");
+    pattern: "{controller=Home}/{action=Register}/{id?}");
 
 app.Run();
